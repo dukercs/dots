c0668d3871eb05aefab01db0ab7123e51543b6f2
dukercs <dukercs@gmail.com>
2023-03-09 16:11:39 -0300

    Primeiro envio de confs

---
+++ ubuquarto/polybar/hack1bar/scripts/upnotifier.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+var=$(cat /var/lib/update-notifier/updates-available|grep -E ^[0-9]|sed -r '/^[\s\t]*$/d'|head -1|cut -d" " -f1) 
+
+if [ $var -gt 0 ] 
+	then 
+		echo ""
+	else 
+		echo ""
+fi
