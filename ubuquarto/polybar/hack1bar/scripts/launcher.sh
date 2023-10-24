762734226ad572469f6b73c019ee681cc65b95b3
dukercs <dukercs@gmail.com>
2023-10-24 11:49:03 -0300

    24-10-2023

--- ubuquarto/polybar/hack1bar/scripts/launcher.sh
+++ ubuquarto/polybar/hack1bar/scripts/launcher.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/polybar/hack1bar/scripts/rofi/launcher.rasi
+#rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/polybar/hack1bar/scripts/rofi/launcher.rasi
+rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/polybar/colorblocks/scripts/rofi/launcher.rasi
+
